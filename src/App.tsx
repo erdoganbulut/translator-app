686d80c6ee8d85acf51e554e284add4a83b19a15
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2022-08-15 03:06:04 +0300

    Create app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Router from './routes';
+import AppHeader from './components/shareds/AppHeader';
+
 import './styles/vendors.scss';
+import './styles/global.scss';
 
 const App = () => (
   <div className="App">
     <main>
+      <AppHeader />
       <p>App Works!</p>
       <Router />
     </main>
